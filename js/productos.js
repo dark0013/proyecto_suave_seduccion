f42ec4cfb370b1abef541f01af2137615356ce20
crianlop <crianlop@espol.edu.ec>
2021-11-26 01:16:29 -0500

    Agregando carousel de ejemplo, pagina de contactos, y pagina de tienda online, con sus respectivos css, scrips, etc
    Agregando carousel de ejemplo, pagina de contactos, y pagina de tienda online, con sus respectivos css, scrips, etc

---
+++ js/productos.js
@@ -0,0 +1,100 @@
+var x, i, j, l, ll, selElmnt, a, b, c;
+/*look for any elements with the class "custom-select":*/
+x = document.getElementsByClassName("custom-select");
+l = x.length;
+for (i = 0; i < l; i++) {
+  selElmnt = x[i].getElementsByTagName("select")[0];
+  ll = selElmnt.length;
+  /*for each element, create a new DIV that will act as the selected item:*/
+  a = document.createElement("DIV");
+  a.setAttribute("class", "select-selected");
+  b = document.createElement("p")
+  b.setAttribute("class","selectText")
+  a.innerHTML = selElmnt.options[selElmnt.selectedIndex].innerHTML;
+  x[i].appendChild(a);
+  /*for each element, create a new DIV that will contain the option list:*/
+  b = document.createElement("DIV");
+  b.setAttribute("class", "select-items select-hide");
+  for (j = 1; j < ll; j++) {
+    /*for each option in the original select element,
+    create a new DIV that will act as an option item:*/
+    c = document.createElement("DIV");
+    selectText = document.createElement("p");
+    selectText.setAttribute("class","selectText")
+    selectText.innerHTML = selElmnt.options[j].innerHTML;
+    c.appendChild(selectText);
+
+    //c.innerHTML = selElmnt.options[j].innerHTML;
+    c.addEventListener("click", function(e) {
+        /*when an item is clicked, update the original select box,
+        and the selected item:*/
+        var y, i, k, s, h, sl, yl;
+        s = this.parentNode.parentNode.getElementsByTagName("select")[0]
+        sl = s.length;
+        h = this.parentNode.previousSibling;
+        for (i = 0; i < sl; i++) {
+          textValue = this.getElementsByClassName("selectText")[0].innerHTML
+          if (s.options[i].innerHTML == textValue) {
+            s.selectedIndex = i;
+            h.innerHTML = textValue;
+            y = this.parentNode.getElementsByClassName("same-as-selected");
+            yl = y.length;
+            for (k = 0; k < yl; k++) {
+              y[k].removeAttribute("class");
+            }
+            this.setAttribute("class", "same-as-selected");
+            break;
+          }
+        }
+        h.click();
+    });
+    b.appendChild(c);
+  }
+  x[i].appendChild(b);
+  a.addEventListener("click", function(e) {
+      /*when the select box is clicked, close any other select boxes,
+      and open/close the current select box:*/
+      e.stopPropagation();
+      closeAllSelect(this);
+      this.nextSibling.classList.toggle("select-hide");
+      this.classList.toggle("select-arrow-active");
+    });
+}
+function closeAllSelect(elmnt) {
+  /*a function that will close all select boxes in the document,
+  except the current select box:*/
+  var x, y, i, xl, yl, arrNo = [];
+  x = document.getElementsByClassName("select-items");
+  y = document.getElementsByClassName("select-selected");
+  xl = x.length;
+  yl = y.length;
+  for (i = 0; i < yl; i++) {
+    if (elmnt == y[i]) {
+      arrNo.push(i)
+    } else {
+      y[i].classList.remove("select-arrow-active");
+    }
+  }
+  for (i = 0; i < xl; i++) {
+    if (arrNo.indexOf(i)) {
+      x[i].classList.add("select-hide");
+    }
+  }
+}
+/*if the user clicks anywhere outside the select box,
+then close all select boxes:*/
+document.addEventListener("click", closeAllSelect);
+
+function favoriteProduct(heart){
+  
+  if(heart.classList[1]== "unchecked"){
+    heart.setAttribute("class","fav checked");
+    heart.setAttribute("src","../../resource/icons/favChecked.png")
+  }
+  else{
+    heart.setAttribute("class","fav unchecked");
+    heart.setAttribute("src","../../resource/icons/favUnchecked.png")
+    
+  }
+
+}
